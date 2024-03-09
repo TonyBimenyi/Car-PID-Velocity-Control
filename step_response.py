f50b240bff8d0dc94468c0705e2c73038ef48939
TonyBimenyi <83509726+TonyBimenyi@users.noreply.github.com>
2024-03-09 20:59:53 +0800

    simulation 1

--- step_response.py
+++ step_response.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.integrate import odeint
 
 # DEFINE MODEL
 
@@ -12,11 +13,11 @@ def vehicule(v,t,u,load):
     Cd = 0.24   #drag coefficient
     rho = 1.225 #air density (kg/m^3)
     A = 5.0     #cross sectionnal area (m^2)
-    Fp = 50    #thrust parameter (N/%pedal)
+    Fp = 30    #thrust parameter (N/%pedal)
     m = 500     #vehicule mass(Kg)
 
     # CALCULATE DERIVATIVE EF VELOCITY
-    dv_dt = (1.0/(m+load)) * (Fp*u - 0.5*rho*Cd*A*v^2)
+    dv_dt = (1.0/(m+load)) * (Fp*u - 0.5*rho*Cd*A*v**2)
     return dv_dt
 
 
@@ -27,7 +28,7 @@ ts = np.linspace(0,tf,nsteps)   # linearly spaced time vector
 
 #Simulate step test operation
 step = np.zeros(nsteps) # u = valve % open
-step[11:] = 50.0    #step up pedal
+step[11:] = 25.0    #step up pedal
 #passengers + cargo load
 load = 200.0 # Kg
 #velocity initial condition
@@ -42,21 +43,22 @@ for i in range(nsteps-1):
     # %clip inputs to -50% to 100%
     if u >= 100.0:
         u = 100.0 
-    if u <= 50.0:
+    if u <= -50.0:
         u = -50.0
     v = odeint(vehicule, v0, [0,delta_t], args=(u,load))
     v0 = v[-1]  #take the last value
+    vs[i+1] = v0 # store the velocity for plotting
 
 
 # plot results
-plt.figue()
+plt.figure()
 plt.subplot(2,1,1)
-plt.plot(ts,vs,'-b',linewidth=3)
+plt.plot(ts,vs,'b-',linewidth=3)
 plt.plot([0, tf],[25,25],'k--',linewidth=2)
 plt.ylabel('Velocity (m/s)')
 plt.legend(['Velocity','Set Point'], loc=2)
 plt.subplot(2,1,2)
-plt.plot(ts,step,'--r', linewidth=3)
+plt.plot(ts,step,'r--', linewidth=3)
 plt.ylabel('Gas Pedal')
 plt.legend(['GasPedal (%)'])
 plt.xlabel('Time (sec)')
